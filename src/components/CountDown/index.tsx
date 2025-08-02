4db180fbc416e11b6531cd5301207345a48a3657
Matheus Griza <matheusgriza@sou.faccat.br>
2025-08-02 13:11:03 -0300

    Implemented CountDown component

---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+type CountDownProps = {
+  children: React.ReactNode;
+};
+
+export function CountDown() {
+  return <div className={styles.container}>00:00</div>;
+}
