739e36f3a7f62cc7f83106b2bf260a1dc71cb526
Matheus Griza <matheusgriza@sou.faccat.br>
2025-08-03 19:28:08 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemain: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
   return <Home />;
