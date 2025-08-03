4552e87a44469656c8149adcce2b3c9cf55f43c1
Matheus Griza <matheusgriza@sou.faccat.br>
2025-08-03 16:15:21 -0300

    Builded template and pages folder for main structure layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,8 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './pages/Home';
 
-import './styles/global.css';
 import './styles/theme.css';
-import { Footer } from './components/Footer';
+import './styles/global.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              type='text'
-              id='abc12345'
-              labelText='Task'
-              required
-              defaultValue='Estudar React'
-              placeholder='Task'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,9 +1,5 @@
 import styles from './styles.module.css';
 
-// type CountDownProps = {
-//   children: React.ReactNode;
-// };
-
 export function CountDown() {
   return <div className={styles.container}>00:00</div>;
 }
